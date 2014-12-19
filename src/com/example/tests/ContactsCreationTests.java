9c56f4379e9e543660ad901397e322a765c4b002
octofox <shinichocoo@gmail.com>
2014-12-19 23:30:41 +0600

    Lazy init of helpers

--- src/com/example/tests/ContactsCreationTests.java
+++ src/com/example/tests/ContactsCreationTests.java
@@ -5,13 +5,13 @@ import org.testng.annotations.Test;
 public class ContactsCreationTests extends TestBase {
 	@Test
 	public void testContactCreation() throws Exception {
-		app.navigationHelper.openMainPage();
-		app.contactHelper.gotoAddContactPage();
-		app.contactHelper.fillContactForm(new ContactData("Anya", "Bagina", "Nsk", "8111111",
+		app.getNavigationHelper().openMainPage();
+		app.getContactHelper().gotoAddContactPage();
+		app.getContactHelper().fillContactForm(new ContactData("Anya", "Bagina", "Nsk", "8111111",
 				"8-000-000-00-00", "123456", "email@mail.ru",
 				"sqrt2@qwerty.tld", "17", "January", "1991", "Rob", "Nsk 2",
 				"Home"));
-		app.contactHelper.submitContactCreation();
-		app.contactHelper.returntoHomePage();
+		app.getContactHelper().submitContactCreation();
+		app.getContactHelper().returntoHomePage();
 	}
 }
--- src/com/example/tests/GroupsCreationTests.java
+++ src/com/example/tests/GroupsCreationTests.java
@@ -5,25 +5,25 @@ import org.testng.annotations.Test;
 public class GroupsCreationTests extends TestBase {
 	@Test
 	public void testNoneEmptyGroupCreation() throws Exception {
-		app.navigationHelper.openMainPage();
-		app.navigationHelper.gotoGroupsPage();
-		app.groupHelper.initGroupCreation();
+		app.getNavigationHelper().openMainPage();
+		app.getNavigationHelper().gotoGroupsPage();
+		app.getGroupHelper().initGroupCreation();
 		GroupData group = new GroupData();
 		group.groupname = "group 1";
 		group.header = "header 1";
 		group.footer = "footer 1";
-		app.groupHelper.fillGroupForm(group);
-		app.groupHelper.submitGroupCreation();
-		app.groupHelper.returntoGroupsPage();
+		app.getGroupHelper().fillGroupForm(group);
+		app.getGroupHelper().submitGroupCreation();
+		app.getGroupHelper().returntoGroupsPage();
 	}
 
 	@Test
 	public void testEmptyGroupCreation() throws Exception {
-		app.navigationHelper.openMainPage();
-		app.navigationHelper.gotoGroupsPage();
-		app.groupHelper.initGroupCreation();
-		app.groupHelper.fillGroupForm(new GroupData("", "", ""));
-		app.groupHelper.submitGroupCreation();
-		app.groupHelper.returntoGroupsPage();
+		app.getNavigationHelper().openMainPage();
+		app.getNavigationHelper().gotoGroupsPage();
+		app.getGroupHelper().initGroupCreation();
+		app.getGroupHelper().fillGroupForm(new GroupData("", "", ""));
+		app.getGroupHelper().submitGroupCreation();
+		app.getGroupHelper().returntoGroupsPage();
 	}
 }

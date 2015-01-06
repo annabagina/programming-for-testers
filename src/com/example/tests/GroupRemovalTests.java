98fdd7347432ea5303e7958a2e5de6670848a068
octofox <shinichocoo@gmail.com>
2015-01-06 17:23:46 +0600

    Compare oldList with newList in Groups tests
    sort, hashCode, CompareTo, toLowerCase

--- src/com/example/tests/GroupRemovalTests.java
+++ src/com/example/tests/GroupRemovalTests.java
@@ -1,5 +1,10 @@
 package com.example.tests;
 
+import static org.testng.Assert.assertEquals;
+
+import java.util.Collections;
+import java.util.List;
+
 import org.testng.annotations.Test;
 
 public class GroupRemovalTests extends TestBase {
@@ -7,7 +12,20 @@ public class GroupRemovalTests extends TestBase {
 	public void deleteSomeGroups() {
 		app.getNavigationHelper().openMainPage();
 		app.getNavigationHelper().gotoGroupsPage();
-		app.getGroupHelper().delete(1);
+		
+		//save old state
+		List<GroupData> oldList = app.getGroupHelper().getGroups();
+		
+		//actions
+		app.getGroupHelper().delete(0);
 		app.getGroupHelper().returntoGroupsPage();
+		
+		//save new state
+		List<GroupData> newList = app.getGroupHelper().getGroups();
+		//compare states
+				
+		oldList.remove(0);
+		Collections.sort(oldList);
+		assertEquals(newList, oldList);
 	}
 }
--- src/com/example/tests/GroupsCreationTests.java
+++ src/com/example/tests/GroupsCreationTests.java
@@ -1,5 +1,10 @@
 package com.example.tests;
 
+import java.util.Collections;
+import java.util.List;
+
+import static org.testng.Assert.assertEquals;
+
 import org.testng.annotations.Test;
 
 public class GroupsCreationTests extends TestBase {
@@ -7,23 +12,48 @@ public class GroupsCreationTests extends TestBase {
 	public void testNoneEmptyGroupCreation() throws Exception {
 		app.getNavigationHelper().openMainPage();
 		app.getNavigationHelper().gotoGroupsPage();
+		
+		//save old state
+		List<GroupData> oldList = app.getGroupHelper().getGroups();
+		//actions		
 		app.getGroupHelper().initGroupCreation();
 		GroupData group = new GroupData();
-		group.groupname = "group 1";
+		group.groupname = "aaa";
 		group.header = "header 1";
 		group.footer = "footer 1";
 		app.getGroupHelper().fillGroupForm(group);
 		app.getGroupHelper().submitGroupCreation();
 		app.getGroupHelper().returntoGroupsPage();
+		
+		//save new state
+		List<GroupData> newList = app.getGroupHelper().getGroups();
+		//compare states
+				
+		oldList.add(group);
+		Collections.sort(oldList);
+		assertEquals(newList, oldList);
 	}
 
 	@Test
 	public void testEmptyGroupCreation() throws Exception {
 		app.getNavigationHelper().openMainPage();
 		app.getNavigationHelper().gotoGroupsPage();
+		
+		//save old state
+		List<GroupData> oldList = app.getGroupHelper().getGroups();
+		
+		//actions
+		GroupData group = new GroupData("", "", "");
 		app.getGroupHelper().initGroupCreation();
-		app.getGroupHelper().fillGroupForm(new GroupData("", "", ""));
+		app.getGroupHelper().fillGroupForm(group);
 		app.getGroupHelper().submitGroupCreation();
 		app.getGroupHelper().returntoGroupsPage();
+		//save new state
+		List<GroupData> newList = app.getGroupHelper().getGroups();
+		//compare states
+				
+		oldList.add(group);
+		Collections.sort(oldList);
+		assertEquals(newList, oldList);
 	}
 }
